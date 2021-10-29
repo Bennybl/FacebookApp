2f8bc360ce08d87b88235d87c12ca93f444e21bb
Benny's xps <benny.blaustein@post.idc.ac.il>
2021-10-29 13:27:04 +0300

    first commit

--- FacebookWinFormsApp/Program.cs
+++ FacebookWinFormsApp/Program.cs
@@ -8,6 +8,7 @@ namespace BasicFacebookFeatures
 {
     static class Program
     {
+        ///github test
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
